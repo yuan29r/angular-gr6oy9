2043d840ac9da62c7592b8ea7f17f550099f0be5
yuan29r <sheng29r@gmail.com>
2019-12-23 13:47:03 +0800

    1 file modified

--- src/app/cart/cart.component.ts
+++ src/app/cart/cart.component.ts
@@ -10,7 +10,9 @@ import { CartService } from '../cart.service';
 export class CartComponent implements OnInit {
   items;
   checkoutForm;
+
   //注入CartService
+  //FormBuilder注入
   constructor(
     private cartService: CartService,
     private formBuilder: FormBuilder,
@@ -20,13 +22,17 @@ export class CartComponent implements OnInit {
       name: '',  address: '' });
 
   }
+
+  //定義onSubmit()處理表單的方法
   onSubmit(customerData) {
     // Process checkout data here
     console.warn('Your order has been submitted', customerData);
-
+    
+    //完成後，清除資料
     this.items = this.cartService.clearCart();
     this.checkoutForm.reset();
   }
+
   ngOnInit() {
      
   }
